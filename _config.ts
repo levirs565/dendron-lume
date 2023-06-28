c2b0411adc211c11b0e2d96f10bbfe91d24135fc
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2023-06-28 21:43:18 +0700

    feat: add meta plugin

--- _config.ts
+++ _config.ts
@@ -6,6 +6,7 @@ import typography from "npm:@tailwindcss/typography@0.5.9";
 import markdownItAsciiMath from "npm:@widcardw/markdown-it-asciimath@0.5.5";
 import toc from "https://deno.land/x/lume_markdown_plugins@v0.5.0/toc.ts";
 import * as tocAnchor from "https://deno.land/x/lume_markdown_plugins@v0.5.0/toc/anchors.ts";
+import metas from "lume/plugins/metas.ts";
 // minifyHTML make html broken some time,
 // probably because of katex
 // import minifyHTML from "lume/plugins/minify_html.ts";
@@ -61,6 +62,7 @@ site.use(
 );
 site.use(postcss());
 site.use(esbuild());
+site.use(metas());
 // site.use(minifyHTML());
 
 export default site;
